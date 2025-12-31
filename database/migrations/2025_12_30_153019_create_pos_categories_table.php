62f832a9eb78f2de180d6ccbc3fdd944fb2e68fd
Marlou Dev <marlou.developer@gmail.com>
2025-12-31 17:42:27 +0800

    added adjusmtent

--- database/migrations/2025_12_30_153019_create_pos_categories_table.php
+++ database/migrations/2025_12_30_153019_create_pos_categories_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
--- database/migrations/2025_12_30_153045_create_pos_units_table.php
+++ database/migrations/2025_12_30_153045_create_pos_units_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('pos_units', function (Blueprint $table) {
             $table->id();
-              $table->string('name')->nullable();
+            $table->string('name')->nullable();
             $table->timestamps();
         });
     }
