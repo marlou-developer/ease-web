14500ce2ddeb6467447b3b8fea51380fdab44210
Marlou Dev <marlou.developer@gmail.com>
2026-01-02 22:49:24 +0800

    pos adjustment

--- resources/js/app/pages/account/pos/products/page.jsx
+++ resources/js/app/pages/account/pos/products/page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../layout";
 import ProductHeaderSection from "./_sections/product-header-section";
 import ProductSearchSection from "./_sections/product-search-section";
@@ -6,6 +6,10 @@ import ProductTableSection from "./_sections/product-table-section";
 import ProductPaginationSection from "./_sections/product-pagination-section";
 
 export default function Page() {
+
+    useEffect(()=>{
+
+    },[])
     return (
         <Layout>
             <div className=" bg-slate-100 font-sans text-slate-700">
